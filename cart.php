23095557f0aef86249a869a8b743b4f75f4257c6
Wietse <wietsedaanvandijk@hotmail.com>
2022-11-15 10:30:56 +0100

    Update cart.php
    LET OP: Op het moment word er een product (productID 1) geforceerd in de cart array
    
    Dumpt alle info over wat er in het winkelmandje staat

--- cart.php
+++ cart.php
@@ -6,6 +6,29 @@ include "header.php";
     <div class="cart-wrapper">
         <h1 class="s-heading">Winkelmandje</h1>
     </div>
+    <?php
+    getCart();
+    //TEST: Handmatig product geforceerd in cart array
+    $cart[1] = 1;
+
+
+    foreach($cart as $productID => $productAmount) {
+        $StockItem = getStockItem($productID, $databaseConnection);
+        $StockItemImage = getStockItemImage($productID, $databaseConnection);
+
+        foreach($StockItem as $test1 => $test2){
+            print("$test1 => $test2 <br>");
+        }
+
+        print("<br><br>");
+
+        print("ProductID: " . $StockItem['StockItemID']) . "<br>";
+        print("Naam: " . $StockItem['StockItemName'] . "<br>");
+        print ("Prijs: " . sprintf("€ %.2f", $StockItem['SellPrice']));
+    }
+        ?>
+
+
 </section>
 
 <?php
