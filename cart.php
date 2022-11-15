b27db5e0ed4861894ff07a75055e0c29a0f4c2f6
Wietse <wietsedaanvandijk@hotmail.com>
2022-11-15 13:26:58 +0100

    Begin opmaak winkelmand

--- cart.php
+++ cart.php
@@ -12,24 +12,42 @@ include "header.php";
 //    $cart[1] = 1;
 //    $cart[12] = 1;
 
-if(isset($cart)){
-    foreach($cart as $productID => $productAmount) {
-        $StockItem = getStockItem($productID, $databaseConnection);
-        $StockItemImage = getStockItemImage($productID, $databaseConnection);
-
-        foreach($StockItem as $test1 => $test2){
+if(isset($cart)) {
+    foreach ($cart as $productID => $productAmount) {
+        $StockItem = getStockItem($productID, $databaseConnection); //Haal de gegevens op van huidige productID en sla op in een array
+        $StockItemImage = getStockItemImage($productID, $databaseConnection); //Haal foto(s) op van huidige productID en sla op in array
+        /*Foto's zijn opgeslagen in een 3D array, elke foto heeft een key beginnend bij 0, als value een array met key ImagePath en als value het pad naar de foto*/
+        foreach ($StockItem as $test1 => $test2) { //DEBUG, laat alle gegevens van een product zien
             print("$test1 => $test2 <br>");
         }
+        // print_r($StockItemImage);
 
-        print("<br><br>");
+        if(isset($StockItemImage[0])){ //Check of een product foto's heeft
+            $productImage = $StockItemImage[0]['ImagePath']; //Sla de 1ste foto van een product op in productImage
+        } else{
+            $productImage = "yoda.png"; //Gebruik een andere foto als placeholder
+        }
+        /*print("<br><br>");
 
         print("ProductID: " . $StockItem['StockItemID']) . "<br>";
         print("Naam: " . $StockItem['StockItemName'] . "<br>");
         print ("Prijs: " . sprintf("€ %.2f", $StockItem['SellPrice']));
 
-        print("<br><br>");
+        print("<br><br>");*/
+        ?>
+            <br>
+        <div class="Cart">
+            <div class="CartItem">
+                <img src="Public/StockItemIMG/<?php print $productImage; ?>">
+                <p><?php print($StockItem['StockItemID'])?></p>
+                <h2><?php print($StockItem['StockItemName'])?></h2>
+                <h3><?php print(sprintf("€ %.2f", $StockItem['SellPrice']))?></h3>
+            </div>
+        </div>
+
+            <?php
     }
-    }
+}
         ?>
 
 
