8435d53359a8fa5b51f4627aa692ef2ca0ca0cd3
robd36 <robdrost1@outlook.com>
2022-11-29 09:47:52 +0100

    tel

--- afrekenen.php
+++ afrekenen.php
@@ -7,7 +7,7 @@ include "header.php";
     <input type="text" id="tussenv" name="tussenv" placeholder="tussenvoegsel"><br>
     <input type="text" id="achternaam" name="achternaam" placeholder="achternaam"><br>
     <input type="text" id="email" name="email" placeholder="email"><br>
-    <input type="number" id="tel" name="tel" placeholder="telefoonnummer"><br>
+    <input type="tel" id="tel" name="tel" placeholder="telefoonnummer"><br>
     <input type="text" id="adres" name="adres" placeholder="straat + huisnummer"><br>
     <input type="text" id="postcode" name="postcode" placeholder="postcode"><br>
     <input type="text" id="woonplaats" name="woonplaats" placeholder="woonplaats"><br>
