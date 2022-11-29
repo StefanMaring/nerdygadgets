aab6cefda4643d511286861a06b2ae23f9eb15ba
StefanMaring <smaring@ziggo.nl>
2022-11-29 09:08:42 +0100

    Update afrekenen.php

--- afrekenen.php
+++ afrekenen.php
@@ -1,7 +1,7 @@
 <?php
 include "header.php";
 ?>
-<form>
+<form method="POST" action="" class="afreken-form" id="afreken-form">
     <label for="voornaam">Voornaam:</label><br>
     <input type="text" id="voornaam" name="voornaam"><br>
     <label for="achternaam">Achternaam:</label><br>
