18e9cef8f804c498b5c035502e74dc8a08f1ed1b
AlexSchoute <s1188065@student.windesheim.nl>
2022-11-29 12:12:45 +0100

    CSS bestelpagina, verzendkosten

--- afrekenen.php
+++ afrekenen.php
@@ -5,32 +5,42 @@ $totaalPrijs = $_SESSION['totaalPrijs'];
 ?>
 
 
-<div class="bezorgadres" id="CenteredContent">
-    <h1 class="default-margin">Bezorgadres</h1>
-    <form action="afrekenen-db.php" method="post" class="default-margin">
-        <input class="stand-input" type="text" id="voornaam" name="voornaam" placeholder="voornaam">
-        <input class="small-input" type="text" id="tussenvoegsel" name="tussenvoegsel" placeholder="tussenvoegsel">
-        <input class="stand-input" type="text" id="achternaam" name="achternaam" placeholder="achternaam"><br><br>
-        <input class="stand-input" type="text" id="email" name="email" placeholder="email"><br><br>
-        <input class="small-input" type="tel" id="tel" name="tel" placeholder="telefoonnummer"><br><br>
-        <input class="stand-input" type="text" id="adres" name="adres" placeholder="straat + huisnummer"><br><br>
-        <input class="small-input" type="text" id="postcode" name="postcode" placeholder="postcode"><br><br>
-        <input class="stand-input" type="text" id="woonplaats" name="woonplaats" placeholder="woonplaats"><br><br>
+<div id="CenteredContent" class="flexBox" >
+    <div class="achtergrond bezorgadres-breedte">
+        <h1 class="default-margin">Bezorgadres</h1>
+        <form action="afrekenen-db.php" method="post" class="default-margin">
+            <input class="stand-input" type="text" id="voornaam" name="voornaam" placeholder="voornaam">
+            <input class="small-input" type="text" id="tussenvoegsel" name="tussenvoegsel" placeholder="tussenvoegsel"><br><br>
+            <input class="stand-input" type="text" id="achternaam" name="achternaam" placeholder="achternaam"><br><br>
+            <input class="stand-input" type="text" id="email" name="email" placeholder="email"><br><br>
+            <input class="stand-input" type="tel" id="tel" name="tel" placeholder="telefoonnummer"><br><br>
+            <input class="stand-input" type="text" id="adres" name="adres" placeholder="straat + huisnummer">
+            <input class="small-input" type="text" id="postcode" name="postcode" placeholder="postcode"><br><br>
+            <input class="stand-input" type="text" id="woonplaats" name="woonplaats" placeholder="woonplaats"><br><br>
+    </div>
 
-
-
-        <div class="overzicht" id="CenteredContent">
+    <div class="achtergrond-wrapper">
+        <div class="achtergrond">
             <h1 class="default-margin">Overzicht</h1>
             <?php
-            print("<p>Prijs: €$totaalPrijs </p>" . "<p>Verzendkosten: </p>" . "<p>Totaalprijs: </p>")
+            $verzendkosten = 0;
+            if($totaalPrijs < 50) {
+                $verzendkosten += 6.95;
+            }
+
+            $eindPrijs = $verzendkosten + $totaalPrijs;
+
+            print("<p>Prijs: €$totaalPrijs </p>" . "<p>Verzendkosten: €$verzendkosten</p>" . "<p>Totaalprijs: €$eindPrijs</p>")
             ?>
         </div>
 
-        <div class="betaalmethode" id="CenteredContent">
+
+        <div class="achtergrond bestellen-hoogte">
             <h1 class="default-margin">Betaalmethode</h1>
-                <input class="stand-input" type="submit" id="bestellen" name="bestellen" value="Bestelling plaatsen"><br>
+                <input type="submit" id="bestellen" name="bestellen" value="Bestelling plaatsen"><br>
             </form>
         </div>
+    </div>
 </div>
 
 <?php
