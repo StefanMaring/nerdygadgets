f309a19630b7ae046846d16ad456e7ef3901c76e
AlexSchoute <s1188065@student.windesheim.nl>
2022-11-29 10:58:25 +0100

    Gegevens doorgeven
    CSS is verneukt nu maar fix ik (hopelijk)

--- afrekenen.php
+++ afrekenen.php
@@ -1,17 +1,36 @@
 <?php
 include "header.php";
 ?>
-<form method="POST" action="afrekenen-db.php">
-    <input type="text" id="voornaam" name="voornaam" placeholder="voornaam"><br>
-    <input type="text" id="tussenvoegsel" name="tussenvoegsel" placeholder="tussenvoegsel"><br>
-    <input type="text" id="achternaam" name="achternaam" placeholder="achternaam"><br>
-    <input type="text" id="email" name="email" placeholder="email"><br>
-    <input type="tel" id="tel" name="tel" placeholder="telefoonnummer"><br>
-    <input type="text" id="adres" name="adres" placeholder="straat + huisnummer"><br>
-    <input type="text" id="postcode" name="postcode" placeholder="postcode"><br>
-    <input type="text" id="woonplaats" name="woonplaats" placeholder="woonplaats"><br>
-    <input type="submit" id="bestellen" name="bestellen" value="Bestelling plaatsen"><br>
-</form>
+
+
+<div class="bezorgadres" id="CenteredContent">
+    <h1 class="default-margin">Bezorgadres</h1>
+    <form action="afrekenen-db.php" method="post" class="default-margin">
+        <input class="stand-input" type="text" id="voornaam" name="voornaam" placeholder="voornaam">
+        <input class="small-input" type="text" id="tussenvoegsel" name="tussenvoegsel" placeholder="tussenvoegsel">
+        <input class="stand-input" type="text" id="achternaam" name="achternaam" placeholder="achternaam"><br><br>
+        <input class="stand-input" type="text" id="email" name="email" placeholder="email"><br><br>
+        <input class="small-input" type="tel" id="tel" name="tel" placeholder="telefoonnummer"><br><br>
+        <input class="stand-input" type="text" id="adres" name="adres" placeholder="straat + huisnummer"><br><br>
+        <input class="small-input" type="text" id="postcode" name="postcode" placeholder="postcode"><br><br>
+        <input class="stand-input" type="text" id="woonplaats" name="woonplaats" placeholder="woonplaats"><br><br>
+
+
+
+        <div class="overzicht" id="CenteredContent">
+            <h1 class="default-margin">Overzicht</h1>
+            <?php
+            print("<p>Prijs: </p>" . "<p>Verzendkosten: </p>" . "<p>Totaalprijs: </p>")
+            ?>
+        </div>
+
+        <div class="betaalmethode" id="CenteredContent">
+            <h1 class="default-margin">Betaalmethode</h1>
+                <input class="stand-input" type="submit" id="bestellen" name="bestellen" value="Bestelling plaatsen"><br>
+            </form>
+        </div>
+</div>
+
 <?php
 include "footer.php";
 ?>
