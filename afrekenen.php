044640d608bbc8d9e9914f1de3f1f29e0338242c
robd36 <robdrost1@outlook.com>
2022-11-29 09:03:41 +0100

    begin form

--- afrekenen.php
+++ afrekenen.php
@@ -1,1 +1,7 @@
 ?php
+<form>
+    <label for="voornaam">Voornaam:</label><br>
+    <input type="text" id="voornaam" name="voornaam"><br>
+    <label for="achternaam">Achternaam:</label><br>
+    <input type="text" id="achternaam" name="achternaam">
+</form>
