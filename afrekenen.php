cf07a4bf423e9d827afda8b4a0fa47d38abb8e18
StefanMaring <smaring@ziggo.nl>
2022-11-30 11:24:18 +0100

    Quality of life update
    wellicht een beetje onzin

--- afrekenen.php
+++ afrekenen.php
@@ -9,20 +9,20 @@ $totaalPrijs = $_SESSION['totaalPrijs'];
     <div class="achtergrond bezorgadres-breedte">
         <h1 class="default-margin">Bezorgadres</h1>
         <form action="afrekenen-db.php" method="post" class="default-margin">
-            <input class="stand-input" type="text" required id="voornaam" name="voornaam" placeholder="*voornaam">
-            <input class="small-input" type="text"  id="tussenvoegsel" name="tussenvoegsel" placeholder="tussenvoegsel"><br><br>
-            <input class="stand-input" type="text" required id="achternaam" name="achternaam" placeholder="*achternaam"><br><br>
-            <input class="stand-input" type="email" required id="email" name="email" placeholder="*email"><br><br>
-            <input class="stand-input" type="tel" required id="tel" name="tel" placeholder="*telefoonnummer"><br><br>
-            <input class="stand-input" type="text" required id="adres" name="adres" placeholder="*straat + huisnummer">
-            <input class="small-input" type="text" required id="postcode" name="postcode" placeholder="*postcode"><br><br>
-            <input class="stand-input" type="text" required id="woonplaats" name="woonplaats" placeholder="*woonplaats"><br><br>
+            <input class="stand-input" type="text" required id="voornaam" name="voornaam" placeholder="*Voornaam">
+            <input class="small-input" type="text"  id="tussenvoegsel" name="tussenvoegsel" placeholder="Tussenvoegsel"><br><br>
+            <input class="stand-input" type="text" required id="achternaam" name="achternaam" placeholder="*Achternaam"><br><br>
+            <input class="stand-input" type="email" required id="email" name="email" placeholder="*Email"><br><br>
+            <input class="stand-input" type="tel" required id="tel" name="tel" placeholder="*Telefoonnummer"><br><br>
+            <input class="stand-input" type="text" required id="adres" name="adres" placeholder="*Straat + huisnummer">
+            <input class="small-input" type="text" required id="postcode" name="postcode" placeholder="*Postcode"><br><br>
+            <input class="stand-input" type="text" required id="woonplaats" name="woonplaats" placeholder="*Woonplaats"><br><br>
             <p class="notice">* is een vereist veld.</p>
     </div>
 
     <div class="achtergrond-wrapper">
         <div class="achtergrond">
-            <h1 class="default-margin">Overzicht</h1>
+            <h2 class="default-margin">Overzicht</h2>
             <?php
             //Verzendkosten worden niet opgeteld boven de 50 euro
             $verzendkosten = 0;
@@ -40,8 +40,10 @@ $totaalPrijs = $_SESSION['totaalPrijs'];
 
 
         <div class="achtergrond bestellen-hoogte">
-            <h1 class="default-margin">Betaalmethode</h1>
-                <input type="submit" class="bestellen-btn btn-style" id="bestellen" name="bestellen" value="Bestelling plaatsen"><br>
+            <h2 class="default-margin">Betaalmethode</h2>
+                <div class="btn-padding-wrp">
+                    <input type="submit" class="bestellen-btn btn-style" id="bestellen" name="bestellen" value="Bestelling plaatsen"><br>
+                </div>
             </form>
         </div>
     </div>
