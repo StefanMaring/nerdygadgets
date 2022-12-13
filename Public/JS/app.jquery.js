f16565bc2f75bd4643779561c2ec747270cb5133
StefanMaring <smaring@ziggo.nl>
2022-12-13 14:30:46 +0100

    Update app.jquery.js

--- Public/JS/app.jquery.js
+++ Public/JS/app.jquery.js
@@ -1,10 +1,22 @@
 //Opens the dialogbox
 $('#add-btn').on("click", function() {
     $('#dialogbox').toggleClass("show-dialog");
-})
+});
 
 //Opens the overlay behind dialog
 $('#add-btn').on("click", function() {
     $('#overlay').toggleClass("show-overlay");
-})
+});
+
+//als het document laad voert de pop-up uit
+$( document ).ready(function() {    
+    $('#pop-upbox').toggleClass("show-dialog");    
+    $('#kortingscode-pop').toggleClass("show-overlay");
+});
+
+$('#x').on("click", function() {    
+    console.log("click");    
+    $('#pop-upbox').removeClass("show-dialog");    
+    $('#kortingscode-pop').removeClass("show-overlay");
+});
 
