89f3edf95a5f6f4d0daa5b97b53b6dd2e4e8c397
Veravv <vera.van.veenhuizen@gmail.com>
2022-12-21 08:41:30 +0100

    Temperatuur bij gekoelde producten

---
+++ tempsensor.php
@@ -0,0 +1,36 @@
+<?php
+
+function connectToDatabase() {
+    $Connection = null;
+
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set MySQLi to throw exceptions
+    try {
+        $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
+        mysqli_set_charset($Connection, 'latin1');
+        $DatabaseAvailable = true;
+    } catch (mysqli_sql_exception $e) {
+        $DatabaseAvailable = false;
+    }
+    if (!$DatabaseAvailable) {
+        die();
+    }
+
+    return $Connection;
+}
+
+$database = connectToDatabase();
+
+while(TRUE){
+    $Tijd = date("Y-m-d H:i:s");
+    $Temperatuur = rand(100, 400)/100;
+
+    $ColdroomUpdate=mysqli_prepare($database,
+        " UPDATE coldroomtemperatures 
+                SET RecordedWhen = ?, Temperature = ?, ValidFrom = ? 
+                WHERE coldRoomSensorNumber = 5;");
+    mysqli_stmt_bind_param($ColdroomUpdate, 'sds', $Tijd , $Temperatuur, $Tijd );
+    mysqli_stmt_execute($ColdroomUpdate);
+
+    print($Temperatuur . "\n");
+    sleep(3);
+}
