6bfdde3ede1b75335943ba4bf32d62b26a8be25c
StefanMaring <smaring@ziggo.nl>
2022-12-22 15:53:10 +0100

    CSS minified

--- header.php
+++ header.php
@@ -16,7 +16,7 @@ $cart = getCart();
     <script src="Public/JS/resizer.js"></script>
 
     <!-- Style sheets-->
-    <link rel="stylesheet" href="Public/CSS/style.css" type="text/css">
+    <link rel="stylesheet" href="Public/CSS/style.min.css" type="text/css">
     <link rel="stylesheet" href="Public/CSS/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="Public/CSS/typekit.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" 
